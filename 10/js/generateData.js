1292309d1cea45f84cf72360c460c69c70ba2500
Keks <keks@htmlacademy.ru>
2022-04-08 00:59:11 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/generateData.js
@@ -0,0 +1,67 @@
+const similarElementsTemplate = document.querySelector('#card').content.querySelector('.popup');
+
+const objectTypes = {
+  flat: 'Квартира',
+  bungalow: 'Бунгало',
+  house: 'Дом',
+  palace: 'Дворец',
+  hotel: 'Отель',
+};
+
+const getFeatures = (element, features) => {
+  const featuresList = element.querySelectorAll('.popup__feature');
+  const modifiers = features.map((feature) =>
+    `popup__feature--${feature}`);
+  featuresList.forEach((featureListItem) => {
+    const modifier = featureListItem.classList[1];
+
+    if (!modifiers.includes(modifier)) {
+      featureListItem.remove();
+    }
+  });
+};
+
+const getPhotos = (element, hrefPhotos) => {
+  const photosList = element.querySelector('.popup__photos');
+  photosList.querySelector('img').remove();
+
+  hrefPhotos.forEach((hrefPhoto) => {
+    const newPhoto = document.createElement('img');
+    newPhoto.src = hrefPhoto;
+    newPhoto.classList.add('popup__photo');
+    newPhoto.width = '45';
+    newPhoto.height = '40';
+    newPhoto.alt = 'Фотография жилья';
+
+    photosList.append(newPhoto);
+  });
+};
+
+const createPopupElement = ({author, offer}) => {
+  const offerElement = similarElementsTemplate.cloneNode(true);
+  offerElement.querySelector('.popup__avatar').src = author.avatar;
+  offerElement.querySelector('.popup__title').textContent = offer.title;
+  offerElement.querySelector('.popup__text--address').textContent = offer.address;
+  offerElement.querySelector('.popup__text--price').textContent = `${offer.price} ₽/ночь`;
+  offerElement.querySelector('.popup__type').textContent = objectTypes[offer.type];
+  offerElement.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
+  offerElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
+  if ('features' in offer) {
+    getFeatures(offerElement, offer.features);
+  } else {
+    offerElement.querySelector('.popup__features').classList.toggle('hidden');
+  }
+  if ('description' in offer) {
+    offerElement.querySelector('.popup__description').textContent = offer.description;
+  } else {
+    offerElement.querySelector('.popup__description').classList.toggle('hidden');
+  }
+  if ('photos' in offer) {
+    getPhotos(offerElement, offer.photos);
+  } else {
+    offerElement.querySelector('.popup__photos').classList.toggle('hidden');
+  }
+  return offerElement;
+};
+
+export {createPopupElement};
---
+++ 10/js/userFormStartPage.js
@@ -0,0 +1,101 @@
+import {createPopupElement} from './generateData.js';
+
+const form = document.querySelector('.ad-form');
+const filterForm = document.querySelector('.map__filters');
+const allElements = form.querySelectorAll('fieldset');
+const filterElements = filterForm.querySelectorAll('.map__filter');
+const mapFeatures = filterForm.querySelector('.map__features');
+const addressField = document.querySelector('[name="address"]');
+const defaultCoordinates = {
+  lat: 35.65283,
+  lng: 139.83948,
+};
+
+const map = L.map('map-canvas').setView({
+  lat: defaultCoordinates.lat,
+  lng: defaultCoordinates.lng,
+}, 12);
+
+const isActiveMap = () => {
+  L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    },
+  ).addTo(map);
+  form.classList.remove('ad-form--disabled');
+  filterForm.classList.remove('ad-form--disabled');
+  allElements.forEach((element) => element.removeAttribute('disabled'));
+  filterElements.forEach((element) => element.removeAttribute('disabled'));
+  mapFeatures.removeAttribute('disabled');
+};
+
+const isInactiveMap = () => {
+  form.classList.add('ad-form--disabled');
+  filterForm.classList.add('ad-form--disabled');
+  allElements.forEach((element) => element.setAttribute('disabled', 'disabled'));
+  filterElements.forEach((element) => element.setAttribute('disabled', 'disabled'));
+  mapFeatures.setAttribute('disabled', 'disabled');
+  addressField.value = `${defaultCoordinates.lat}, ${defaultCoordinates.lng}`;
+};
+
+document.addEventListener('DOMContentLoaded', isInactiveMap());
+map.on('load', isActiveMap());
+
+const mainIcon = L.icon({
+  iconUrl: './img/main-pin.svg',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+});
+
+const icon = L.icon ({
+  iconUrl: './img/pin.svg',
+  iconSize: [40, 40],
+  iconAnchor: [20, 40],
+});
+
+const markerGroup = L.layerGroup().addTo(map);
+
+const createMarker = (adv) => {
+  const marker = L.marker (
+    {
+      lat: Number(adv.location.lat),
+      lng: Number(adv.location.lng),
+    },
+    {
+      icon,
+    },
+  );
+
+  marker
+    .addTo(markerGroup)
+    .bindPopup(createPopupElement(adv));
+};
+
+const mainMarker = L.marker(
+  {
+    lat: defaultCoordinates.lat,
+    lng: defaultCoordinates.lng,
+  },
+  {
+    draggable: true,
+    icon: mainIcon,
+  },
+);
+
+mainMarker.addTo(map);
+
+const setMainMarker = () => {
+  mainMarker.setLatLng ({
+    lat: defaultCoordinates.lat,
+    lng: defaultCoordinates.lng,
+  });
+  addressField.value = `${defaultCoordinates.lat.toFixed(5)}, ${defaultCoordinates.lng.toFixed(5)}`;
+};
+
+mainMarker.on('moveend', (evt) => {
+  const markerCoordinates = evt.target.getLatLng();
+  addressField.value = `${markerCoordinates.lat.toFixed(5)}, ${markerCoordinates.lng.toFixed(5)}`;
+});
+
+export {createMarker, setMainMarker};
